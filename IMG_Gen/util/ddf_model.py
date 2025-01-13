1e906163b05da3f2ab5c48cfc49a422758fc401d
Gao2546 <athipyuthaworawit@gmail.com>
2025-01-14 00:08:49 +0700

    add_code

--- IMG_Gen/util/ddf_model.py
+++ IMG_Gen/util/ddf_model.py
@@ -9,10 +9,10 @@ def signal_handler(sig, frame):
     torch.cuda.synchronize()
     sys.exit(0)
 def main():
-    model_ckp = 'model/checkpoint/DDPM_T01.pth'
+    model_ckp = None#'model/checkpoint/DDPM_T01.pth'
     signal.signal(signal.SIGINT, signal_handler)
-    # train(checkpoint_path=None, lr=2e-5, batch_size=16, num_epochs=15)
-    inference(model_ckp)
+    train(checkpoint_path=model_ckp, lr=2e-5, batch_size=16, num_epochs=15)
+    # inference(model_ckp)
 
 
 if __name__ == '__main__':
