fca33265214f4e7882fa24806d57bfb1584d03d9
Gao2546 <athipyuthaworawit@gmail.com>
2025-03-16 06:05:42 +0700

    update Image gen code to train DDP

--- IMG_Gen/ddpm_model.py
+++ IMG_Gen/ddpm_model.py
@@ -124,7 +124,7 @@ def main():
 
     # Training setup
     size = 16 * 8
-    batch_size = 16 * 8
+    batch_size = 16 * 4
 
     transform = transforms.Compose([
         transforms.Resize((size, size)),
@@ -134,7 +134,7 @@ def main():
 
     # Load dataset
     # train_dataset = datasets.ImageFolder(root=path_to_data, transform=transform)
-    train_dataset = datasets.CIFAR100(root='IMG_Gen/data', train=True, transform=transform, download=True)
+    train_dataset = datasets.CIFAR100(root='/data', train=True, transform=transform, download=True)
     
     print("Data loaded successfully!")
 
