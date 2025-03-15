f255224b09088532c36606af03b9edb8c036b3ea
Gao2546 <athipyuthaworawit@gmail.com>
2025-03-16 04:34:18 +0700

    update Image gen code to train DDP

--- IMG_Gen/ddpm_model.py
+++ IMG_Gen/ddpm_model.py
@@ -124,7 +124,7 @@ def main():
 
     # Training setup
     size = 16 * 8
-    batch_size = 16 * 12
+    batch_size = 16 * 10
 
     transform = transforms.Compose([
         transforms.Resize((size, size)),
